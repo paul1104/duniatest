494de44145208e8dcb02a91486b66483e293f690
paul1104 <32156061+paul1104@users.noreply.github.com>
2018-06-12 11:58:32 +0700

    Update testbruh.py

--- testbruh.py
+++ testbruh.py
@@ -745,17 +745,17 @@ def clientBot(op):
                                 client.sendMention(to, helpmess, [sender])
                                 Oa = 'ud4082219b6754e7b610f84d07d3b436b'
                                 client.sendContact(to, Oa)
-			    elif cmd == "/siders":
+                            elif cmd == "/siders":
                                 client.sendMention(to, sidersmess, [sender])
-				Oa = 'ud4082219b6754e7b610f84d07d3b436b'
+                                Oa = 'ud4082219b6754e7b610f84d07d3b436b'
                                 client.sendContact(to, Oa)
-			    elif cmd == "/search":
+                            elif cmd == "/search":
                                 client.sendMention(to, searchmess, [sender])
-				Oa = 'ud4082219b6754e7b610f84d07d3b436b'
+                                Oa = 'ud4082219b6754e7b610f84d07d3b436b'
                                 client.sendContact(to, Oa)
-			    elif cmd == "/group":
+                            elif cmd == "/group":
                                 client.sendMention(to, groupmess, [sender])
-				Oa = 'ud4082219b6754e7b610f84d07d3b436b'
+                                Oa = 'ud4082219b6754e7b610f84d07d3b436b'
                                 client.sendContact(to, Oa)
                             elif cmd == "tts":
                                 helpTextToSpeech = helptexttospeech()
